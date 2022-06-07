47c4600bd6aa7fc72f7946fa408b8810c6d47403
Theophile Avoyne <tavoyne@gmail.com>
2022-06-07 16:46:56 +0200

    Move away from iframe

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-const iframeElement = document.querySelector("iframe");
+const connectedElements = document.querySelectorAll("#preview *[data-path]");
 const selectElement = document.querySelector("select");
 
-iframeElement.addEventListener("load", () => {
-  const iframeDocument = iframeElement.contentDocument;
-  const elements = iframeDocument.querySelectorAll("*[data-path]");
-
-  for (const element of elements) {
-    const path = element.getAttribute("data-path");
-    const selector = `*[name="${path}"]`;
-    const input = document.querySelector(selector);
-    
-    if (!input) {
-      console.warn(new Error(`Couldn't find element with [name="${path}"].`));
-    } else {
-      element.textContent = input.value;
-      input.addEventListener("keyup", (evt) => {
-        element.textContent = evt.target.value;
-      });
-    }
+for (const connectedElement of connectedElements) {
+  const path = connectedElement.getAttribute("data-path");
+  const selector = `*[name="${path}"]`;
+  const input = document.querySelector(selector);
+  
+  if (!input) {
+    console.warn(new Error(`No element matches selector ${selector}.`));
+  } else {
+    connectedElement.textContent = input.value;
+    input.addEventListener("keyup", (evt) => {
+      connectedElement.textContent = evt.target.value;
+    });
   }
-});
+}
 
 selectElement.addEventListener("change", (evt) => {
   iframeElement.src = `templates/${evt.target.value}`;
