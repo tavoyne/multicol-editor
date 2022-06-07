3e13270cee0f1e8f275c625ec32d7f936cdc9e2a
Theophile Avoyne <tavoyne@gmail.com>
2022-06-07 15:39:03 +0200

    Minor

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const selectElement = document.querySelector("select");
 iframeElement.addEventListener("load", () => {
   const iframeDocument = iframeElement.contentDocument;
   const elements = iframeDocument.querySelectorAll("*[data-path]");
+
   for (const element of elements) {
     const path = element.getAttribute("data-path");
     const selector = `*[name="${path}"]`;
     const input = document.querySelector(selector);
+    
     if (!input) {
       console.warn(new Error(`Couldn't find element with [name="${path}"].`));
     } else {
